b80626ae6cd269a2ff4eaa22eb31cc43c1b96df9
jgstressertrigo <jgstressertrigo@gmail.com>
2023-11-15 12:18:25 -0300

    começo menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import CRUD.CRUDClientes;
+
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner menu = new Scanner (System.in);
+
+        System.out.print(" *********LocadoraPU*********\n\n");
+        System.out.print(" ----------------------------\n");
+        System.out.print("|  1. Menu Clientes         |\n");
+        System.out.print("|  2. Menu Filmes           |\n");
+        System.out.print("|  3. Menu Genero          |\n");
+        System.out.print("|  4. Menu Locação         |\n");
+        System.out.print(" ----------------------------\n");
+        System.out.print("Escolha uma opção: ");
+
+        int op = menu.nextInt();
+
+
+        switch (op) {
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            default:
+        }
     }
 }
